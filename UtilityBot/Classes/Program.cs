07db737f4a53310c567c1ed49d9b80a8791040d2
Innokod <Osipovinn@list.ru>
2025-07-01 23:22:05 +0300

    Добавьте файлы проекта.

---
+++ UtilityBot/Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UtilityBot.Classes;
+
+namespace VoiceTexterBot
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<User> users = new List<User>();
+            User user1 = new User();
+            user1.IsPremium = false;
+            user1.Login = "user";
+            users.Add(user1);
+            User user2 = new User();
+            user2.IsPremium = true;
+            user2.Login = "gun";
+            users.Add(user2);
+            foreach (User user in users) 
+            {
+                Console.WriteLine($"{user.Login}, введите ваше имя:");
+                user.Name = Console.ReadLine();
+                if (!user.IsPremium)
+                {
+                    User.ShowAds();
+                }
+            }
+        }
+    }
+}
